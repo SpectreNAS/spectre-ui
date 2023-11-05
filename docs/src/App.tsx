64709947f2ae6fd6fc315fe597ca09fe2a92ee21
lingj <lingj.jhf@outlook.com>
2023-11-06 00:24:46 +0800

    修改docs

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,12 +1,14 @@
-import { SpCheckbox } from '@spectre-ui/core'
+import { SpCheckbox, SpConfigProvider } from '@spectre-ui/core'
 import '@spectre-ui/core/styles.css'
 
 function App() {
 
   return (
     <div class=' bg-black top-0 right-0 bottom-0 left-0 absolute'>
+      <SpConfigProvider>
+        <SpCheckbox></SpCheckbox>
+      </SpConfigProvider>
 
-      <SpCheckbox></SpCheckbox>
     </div>
   )
 }
