4037f1326686703aeec63da6310c39021bd28de2
lingj <lingj.jhf@outlook.com>
2023-11-05 23:26:21 +0800

    dev

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,10 +1,12 @@
-import { SpButton, ButtonProps } from '@spectre-ui/core'
+import { SpCheckbox } from '@spectre-ui/core'
 import '@spectre-ui/core/styles.css'
 
 function App() {
+
   return (
-    <div>
-      <SpButton></SpButton>
+    <div class=' bg-black top-0 right-0 bottom-0 left-0 absolute'>
+
+      <SpCheckbox></SpCheckbox>
     </div>
   )
 }
