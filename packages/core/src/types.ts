e2ab7888a1a75a462ba424c9c11030d06617a27f
JHFeng <lingj.jhf@outlook.com>
2023-11-06 10:44:38 +0800

    添加类型

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,3 +1,7 @@
 export type ComponentSize = 'default' | 'small' | 'medium' | 'large'
 
 export type ComponentColor = 'dark' | 'light'
+
+export type ValueChanged<T> = (value: T) => void
+
+export type VoidCallback = () => void
