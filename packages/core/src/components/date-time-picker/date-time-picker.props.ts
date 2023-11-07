7715ac22ec3dd788d7425f03e123684ed86affcf
lingj <lingj.jhf@outlook.com>
2023-11-08 01:12:36 +0800

    初始化DateTimePicker

---
+++ packages/core/src/components/date-time-picker/date-time-picker.props.ts
@@ -0,0 +1,5 @@
+import { ComponentProps } from '../../types'
+
+export interface DateTimePickerProps extends ComponentProps<HTMLDivElement> {
+  
+}
---
+++ packages/core/src/components/date-time-picker/index.ts
@@ -0,0 +1,2 @@
+export { DateTimePicker as SpDateTimePicker } from './DateTimePicker'
+export type { DateTimePickerProps } from './date-time-picker.props'
