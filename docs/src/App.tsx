5810fcd8c754f459b2b04e2db03d374ac559c0ae
JHFeng <lingj.jhf@outlook.com>
2023-11-07 15:46:07 +0800

    dev

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -6,13 +6,14 @@ function App() {
     console.log(value)
   }
   return (
-    <div class=' bg-black top-0 right-0 bottom-0 left-0 absolute'>
+    <div class=' bg-[#1a1a1a] text-white top-0 right-0 bottom-0 left-0 absolute'>
+      Hello
       <SpConfigProvider>
         <div class='flex w-100px justify-between items-end'>
-          <SpButton size='small'></SpButton>
-          <SpButton size='medium'></SpButton>
-          <SpButton></SpButton>
-          <SpButton size='large'></SpButton>
+          <SpButton size='small'>Button</SpButton>
+          <SpButton size='medium'>Button</SpButton>
+          <SpButton type='text' color='primary'>Button</SpButton>
+          <SpButton size='large'>Button</SpButton>
         </div>
         <div class='flex w-100px justify-between items-end'>
           <SpCheckbox size='small'></SpCheckbox>
@@ -21,10 +22,10 @@ function App() {
           <SpCheckbox size='large'></SpCheckbox>
         </div>
         <div class='flex w-200px justify-between items-end'>
-          <SpTag size='small'>Tag</SpTag>
-          <SpTag size='medium'>Tag</SpTag>
-          <SpTag type='light'>Tag</SpTag>
-          <SpTag size='large'>Tag</SpTag>
+          <SpTag size='small' round closable>超小标签</SpTag>
+          <SpTag round size='medium' closable>Tag</SpTag>
+          <SpTag closable round>Tag</SpTag>
+          <SpTag size='large' round color='danger' closable>Tag</SpTag>
         </div>
       </SpConfigProvider>
 
