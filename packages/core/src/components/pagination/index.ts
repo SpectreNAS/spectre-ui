ee4bec213cdf48710383d43a5dbc7d2adecf888d
lingj <lingj.jhf@outlook.com>
2023-11-08 00:54:23 +0800

    初始化Pagination

---
+++ packages/core/src/components/pagination/index.ts
@@ -0,0 +1,2 @@
+export { Pagination as SpPagination } from './Pagination'
+export type { PaginationProps } from './pagination.props'
---
+++ packages/core/src/components/pagination/pagination.props.ts
@@ -0,0 +1,5 @@
+import { ComponentProps } from '../../types'
+
+export interface PaginationProps extends ComponentProps<HTMLDivElement> {
+  
+}
