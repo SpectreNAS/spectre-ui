ecc8f6cfb5e30d03b5046d59abab2be6c32c03b2
JHFeng <lingj.jhf@outlook.com>
2023-11-08 16:34:23 +0800

    修改button参数

--- packages/core/src/components/button/Button.tsx
+++ packages/core/src/components/button/Button.tsx
@@ -1,17 +1,27 @@
-import { mergeProps } from 'solid-js'
-import { ButtonProps } from './button.props'
+import { ButtonProps, generateProps } from './button.props'
 import { mergeClasses } from '../../utils'
 
 export const Button = (propsRaw: ButtonProps) => {
-  const props = mergeProps({ round: false }, propsRaw)
+  const [eventHandlers, props] = generateProps(propsRaw)
 
   const buttonClasses = () => mergeClasses([
     'sp-button',
     props.size ?? '',
     props.type ?? '',
     props.color ?? '',
-    props.round ? 'round' : ''
+    props.round ? 'round' : '',
+    props.class ?? ''
   ])
 
-  return <button class={buttonClasses()}>{props.children}</button>
+  return (
+    <button
+      class={buttonClasses()}
+      classList={props.classList}
+      style={props.style}
+      ref={props.ref}
+      {...eventHandlers}
+    >
+      {props.children}
+    </button>
+  )
 }
--- packages/core/src/components/button/button.props.ts
+++ packages/core/src/components/button/button.props.ts
@@ -1,10 +1,16 @@
+import { mergeProps, splitProps } from 'solid-js'
 import { ComponentSize, ComponentColor, ComponentParentProps } from '../../types'
+import { customEventHandlersName } from '../../events'
 
 export type ButtonType = 'light' | 'text'
 
-export interface ButtonProps extends ComponentParentProps<HTMLSpanElement> {
+export interface ButtonProps extends ComponentParentProps<HTMLButtonElement> {
   type?: ButtonType
   color?: ComponentColor
   size?: ComponentSize
   round?: boolean
+}
+
+export function generateProps(propsRaw: ButtonProps) {
+  return splitProps(mergeProps({ round: false }, propsRaw), customEventHandlersName)
 }
