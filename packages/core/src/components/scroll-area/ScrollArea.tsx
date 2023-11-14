73306ce21b1735fe4c3e6c926e7c34972182cb09
JHFeng <lingj.jhf@outlook.com>
2023-11-14 12:00:32 +0800

    ScrollArea开发

--- packages/core/src/components/scroll-area/ScrollArea.tsx
+++ packages/core/src/components/scroll-area/ScrollArea.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js'
+import { createSignal, onMount } from 'solid-js'
 import { mergeClasses } from '../../utils'
 import { ScrollAreaProps, generateProps } from './scroll-area.props'
 import { SpDraggable } from '../draggable'
@@ -8,54 +8,100 @@ export const ScrollArea = (propsRaw: ScrollAreaProps) => {
 
   const [eventHandlers, props] = generateProps(propsRaw)
 
+  let viewRef: HTMLDivElement | undefined
   const scrollbar = new Scrollbar()
   const [verticalSliderY, setVerticalSliderY] = createSignal(0)
+  const [verticalSliderMaxY, setVerticalSliderMaxY] = createSignal(0)
   const [verticalSliderHeight, setVerticalSliderHeight] = createSignal(0)
   const [horizontalSliderX, setHorizontalSliderX] = createSignal(0)
+  const [horizontalSliderMaxX, setHorizontalSliderMaxX] = createSignal(0)
   const [horizontalSliderWidth, setHorizontalSliderWidth] = createSignal(0)
 
   const scrollAreaClasses = () => mergeClasses([
-    'sp-scroll-area'
+    'sp-scroll-area',
+    props.class ?? ''
   ])
-
   const verticalSliderStyles = () => `height:${verticalSliderHeight()}px;`
-
   const horizontalSliderStyles = () => `width:${horizontalSliderWidth()}px;`
 
+  function setViewRef(el: HTMLDivElement) {
+    viewRef = el
+    onMount(() => {
+      const { width, height } = el.getBoundingClientRect()
+      scrollbar
+        .setContentWidth(el.scrollHeight)
+        .setContentHeight(el.scrollHeight)
+        .setViewWidth(width)
+        .setViewHeight(height)
+      setVerticalSliderY(scrollbar.thumbY)
+      setHorizontalSliderX(scrollbar.thumbX)
+      setVerticalSliderMaxY(scrollbar.viewHeight - scrollbar.thumbHeight)
+      setHorizontalSliderMaxX(scrollbar.viewWidth - scrollbar.thumbWidth)
+      setVerticalSliderHeight(scrollbar.thumbHeight)
+      setHorizontalSliderWidth(scrollbar.thumbWidth)
+    })
+  }
+
+  function setViewScroll(point: Partial<Point>) {
+    if (viewRef?.scrollLeft !== undefined && point.x !== undefined) {
+      viewRef.scrollLeft = point.x
+    }
+
+    if (viewRef?.scrollTop !== undefined && point.y !== undefined) {
+      viewRef.scrollTop = point.y
+    }
+  }
+
   function onVerticalSlider({ y }: Point) {
-    scrollbar.thumbTo({ y })
+    setViewScroll({ y: scrollbar.thumbTo({ y }).scrollY })
   }
 
   function onHorizontalSlider({ x }: Point) {
-    scrollbar.thumbTo({ x })
+    setViewScroll({ x: scrollbar.thumbTo({ x }).scrollX })
   }
 
   function onWheelScroll(event: Event) {
     const target = event.target as HTMLDivElement
     scrollbar.scrollTo({ y: target.scrollTop })
+    setVerticalSliderY(scrollbar.thumbY)
+    setHorizontalSliderX(scrollbar.thumbX)
   }
 
   return (
-    <div class={scrollAreaClasses()}>
-      <div class='sp-scroll-area-view' onScroll={onWheelScroll}>
+    <div
+      class={scrollAreaClasses()}
+      classList={props.classList}
+      style={props.style}
+      ref={props.ref}
+      {...eventHandlers}
+    >
+      <div class='sp-scroll-area-view' ref={setViewRef} onScroll={onWheelScroll}>
         <div>
           {props.children}
         </div>
       </div>
-      <div class='sp-scroll-area-vertical-bar'></div>
-      <SpDraggable y={verticalSliderY()} minY={0} change={onVerticalSlider}>
-        <div
+      <div class='sp-scroll-area-vertical-bar'>
+        <SpDraggable
           class='sp-scroll-area-vertical-slider'
           style={verticalSliderStyles()}
-        ></div>
-      </SpDraggable>
-      <div class='sp-scroll-area-horizontal-bar'></div>
-      <SpDraggable y={horizontalSliderX()} minY={0} change={onHorizontalSlider}>
-        <div
+          only='y'
+          y={verticalSliderY()}
+          minY={0}
+          maxY={verticalSliderMaxY()}
+          change={onVerticalSlider}
+        />
+      </div>
+      <div class='sp-scroll-area-horizontal-bar'>
+        <SpDraggable
           class='sp-scroll-area-horizontal-slider'
           style={horizontalSliderStyles()}
-        ></div>
-      </SpDraggable>
+          only='x'
+          x={horizontalSliderX()}
+          minX={0}
+          maxX={horizontalSliderMaxX()}
+          change={onHorizontalSlider}
+        />
+      </div>
     </div>
   )
 }
