87aa8a7ba6d2f56c9b5c149716239391963e0267
JHFeng <lingj.jhf@outlook.com>
2023-11-22 17:41:54 +0800

    添加TimePicker

--- packages/core/src/components/time-picker/TimePicker.tsx
+++ packages/core/src/components/time-picker/TimePicker.tsx
@@ -1,12 +1,42 @@
+import { createEffect, createSignal } from 'solid-js'
 import { SpInput } from '../input'
 import { SpPopover } from '../popover'
 import { TimePickerPanel } from './TimePickerPanel'
+import { TimePickerProps, generateProps } from './time-picker.props'
+import dayjs from 'dayjs'
 
-export const TimePicker = () => {
+export const TimePicker = (propsRaw: TimePickerProps) => {
+
+  const [eventHandlers, props] = generateProps(propsRaw)
+
+  const [day, setDay] = createSignal(dayjs())
+
+  const inputValue = () => day().format('HH:mm:ss')
+
+  createEffect(() => {
+    setDay(dayjs(props.value))
+  })
+
+  function onChangeTimePicker(value: dayjs.Dayjs) {
+    setDay(value)
+  }
 
   return (
-    <SpPopover renderContent={<TimePickerPanel />}>
-      <SpInput></SpInput>
+    <SpPopover
+      classList={props.classList}
+      style={props.style}
+      trigger='focus'
+      renderContent={
+        <TimePickerPanel
+          value={day()}
+          change={onChangeTimePicker}
+        />
+      }
+    >
+      <SpInput
+        value={inputValue()}
+        {...eventHandlers}
+      ></SpInput>
     </SpPopover>
   )
 }
--- packages/core/src/components/time-picker/time-picker.props.ts
+++ packages/core/src/components/time-picker/time-picker.props.ts
@@ -1,5 +1,18 @@
+import { mergeProps, splitProps } from 'solid-js'
+import { customEventHandlersName } from '../../events'
+import dayjs from 'dayjs'
 import { TimePickerPanelProps } from './time-picker-panel.props'
 
 export interface TimePickerProps extends TimePickerPanelProps {
   disabled?: boolean
+}
+
+export function generateProps(propsRaw: TimePickerPanelProps) {
+  return splitProps(mergeProps({
+    value: dayjs(), 
+    hours: true,
+    minutes: true,
+    seconds: true,
+    disabled: false,
+  }, propsRaw), customEventHandlersName)
 }
