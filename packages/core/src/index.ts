57968888f7d626ec8817a8933a430707b4401df5
JHFeng <lingj.jhf@outlook.com>
2023-11-23 16:44:19 +0800

    导出SystemThemeType

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
 export type { ComponentTheme, ComponentSize } from './types'
 export { getSystemThemeType } from './theme'
-export type { Theme } from './theme'
+export type { Theme, SystemThemeType } from './theme'
 export * from './components'
