b20abd96a89a474f99b94134a1536abae988bd2a
JHFeng <lingj.jhf@outlook.com>
2023-11-24 09:37:31 +0800

    更新docs

--- docs/src/pages/scaffold/Scaffold.tsx
+++ docs/src/pages/scaffold/Scaffold.tsx
@@ -3,19 +3,25 @@ import { Ghost } from '@/components/icon/Ghost'
 import { Menu } from '@/components/icon/Menu'
 import { ThemeSwitch } from '@/components/theme-switch'
 import { Outlet } from '@solidjs/router'
-import { SpIconButton, SpScrollArea } from '@spectre-ui/core'
+import { SpIconButton, SpScrollArea, SpDrawer } from '@spectre-ui/core'
 import { SlideBar } from './SlideBar'
 import { useNavigate } from '@solidjs/router'
+import { createSignal } from 'solid-js'
 
 export const Scaffold = () => {
   const navigate = useNavigate()
-
+  const [visibleSideBarDrawer, setVisibleSideBarDrawer] = createSignal(false)
   return (
     <div class='h-full w-full flex flex-col'>
+      <SpDrawer width='300px' value={visibleSideBarDrawer()} change={setVisibleSideBarDrawer}>
+        <div class='flex flex-col h-full'>
+          <SlideBar />
+        </div>
+      </SpDrawer>
       <div class='flex h-64px box-border w-full border-b border-b-solid border-b-[var(--border-common-default)] flex-shrink-0'>
         <div class='flex px-5 items-center h-full flex-shrink-0'>
           <Ghost class='text-32px cursor-pointer hover:text-[var(--text-brand-hover)] active:text-[var(--text-brand-active)]' onClick={() => navigate('/')}></Ghost>
-          <SpIconButton class='xl:hidden!' size='large' type='text'>
+          <SpIconButton class='xl:hidden!' size='large' type='text' onClick={[setVisibleSideBarDrawer, true]}>
             <Menu />
           </SpIconButton>
         </div>
