5e9c18ea505128274751a3f8565e6f703acdddfc
JHFeng <lingj.jhf@outlook.com>
2023-11-28 15:24:33 +0800

    更新docs

--- docs/src/pages/color-pickers/ColorPickers.tsx
+++ docs/src/pages/color-pickers/ColorPickers.tsx
@@ -1,21 +1,18 @@
-import { SpColorPickerPanel, SpColorSaturation, SpColorHue, SpColorAlpha, Color } from '@spectre-ui/core'
+import { SpColorPickerPanel, SpColorSaturation, SpColorHue, SpColorAlpha } from '@spectre-ui/core'
 import { ComponentPageLayout } from '@/components/layouts'
 import { Anchor } from '@/components/anchor'
 import { Card } from '@/components/card'
 
 export const ColorPickers = () => {
 
-  function change(value: Color) {
-    console.log(value.hexa())
-  }
   return (
     <ComponentPageLayout>
-      {/* <h1>ColorPickers</h1>
+      <h1>ColorPickers</h1>
 
-      <Anchor href='#basic-usage'>基础用法</Anchor> */}
+      <Anchor href='#basic-usage'>基础用法</Anchor>
       <Card>
         <div class=' h-200px mb-4 relative'>
-          <SpColorPickerPanel change={change}>
+          <SpColorPickerPanel>
             <SpColorSaturation />
             <SpColorHue />
             <SpColorAlpha />
