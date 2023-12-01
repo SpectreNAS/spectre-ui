b122d1fa639535f68f583a17a3dcf9b292997ff7
JHFeng <lingj.jhf@outlook.com>
2023-12-01 16:50:58 +0800

    CascadeSelectPanel dev

--- packages/core/src/components/cascade-select/CascadeSelectPanel.tsx
+++ packages/core/src/components/cascade-select/CascadeSelectPanel.tsx
@@ -1,5 +1,163 @@
+import { For, JSX, createEffect, createSignal } from 'solid-js'
 
-export const CascadeSelectPanel = () => {
+import { CascadeSelectPanelProps, generateProps, CascadeSelectOption } from './cascade-select-panel.props'
+import { mergeClasses } from '../../utils'
+import { SpScrollArea } from '../scroll-area'
 
-  return <div></div>
+interface OptionStatus {
+  label?: JSX.Element
+  value: string
+  disabled?: boolean
+
+  parent?: string
+  check: boolean
+  indeterminate: boolean
+  leaf: boolean
+  childrenValues: string[]
+}
+
+type OptionStatusMap = Map<string, OptionStatus>
+
+export const CascadeSelectPanel = (propsRaw: CascadeSelectPanelProps) => {
+  const [eventHandlers, props] = generateProps(propsRaw)
+
+  let optionsStatusMap: OptionStatusMap = new Map()
+  const [selected, setSelected] = createSignal<string[]>([])
+  const [cascades, setCascades] = createSignal<OptionStatus[][]>([])
+
+  const cascadeSelectClasses = () => mergeClasses([
+    'sp-cascade-select-panel',
+    props.class ?? ''
+  ])
+
+  createEffect(() => {
+    optionsStatusMap = resetOptionsStatusMap(props.options)
+    setCascades([
+      getRootOptions(props.options),
+      ...(props.value !== undefined ? generateChildrenCascades(optionsStatusMap, props.value) : [])
+    ])
+  })
+
+  function getRootOptions(options: CascadeSelectOption[]): OptionStatus[] {
+    const rootOptions: OptionStatus[] = []
+    for (const option of options) {
+      const rootOption = optionsStatusMap.get(option.value)
+      if (rootOption) {
+        rootOptions.push(rootOption)
+      }
+    }
+    return rootOptions
+  }
+
+  function onActiveOption(level: number, option: OptionStatus) {
+    const selectedOptions = [...selected()]
+    if (selectedOptions[level] !== option.value) {
+      selectedOptions[level] = option.value
+      const nextOption = selectedOptions[level + 1]
+      if (nextOption && !(optionsStatusMap.get(nextOption)?.parent === option.value)) {
+        selectedOptions.splice(level + 1, selectedOptions.length)
+      }
+      setSelected(selectedOptions)
+      setCascades(value => {
+        const cascades = value.slice(0, level + 1)
+        const children = getChildrenOptions(optionsStatusMap, option.value)
+        if (children.length > 0) {
+          cascades.push(children)
+        }
+        return cascades
+      })
+    }
+  }
+
+  return (
+    <div
+      class={cascadeSelectClasses()}
+      classList={props.classList}
+      style={props.style}
+      ref={props.ref}
+      {...eventHandlers}
+    >
+      <For each={cascades()}>
+        {
+          (options, level) => (
+            <SpScrollArea>
+              <div class='sp-cascade-select-column'>
+                <For each={options}>
+                  {
+                    (option) => (
+                      <div
+                        class='sp-cascade-select-option'
+                        classList={{ active: selected().includes(option.value) }}
+                        onClick={() => onActiveOption(level(), option)}
+                      >
+                        {option.label}
+                      </div>
+                    )
+                  }
+                </For>
+              </div>
+            </SpScrollArea>
+          )
+        }
+      </For>
+
+    </div>
+  )
+}
+
+function resetOptionsStatusMap(options: CascadeSelectOption[], parent?: string): OptionStatusMap {
+  const map = new Map<string, OptionStatus>()
+  for (const option of options) {
+    if (option.children && option.children.length > 0) {
+      resetOptionsStatusMap(option.children, option.value).
+        forEach((value, key) => map.set(key, value))
+    }
+    map.set(
+      option.value,
+      {
+        label: option.label,
+        value: option.value,
+        disabled: option.disabled,
+        parent,
+        check: false,
+        indeterminate: false,
+        leaf: !(option.children && option.children.length > 0),
+        childrenValues: option?.children?.map(child => child.value) ?? [],
+      }
+    )
+  }
+  return map
+}
+
+function generateChildrenCascades(map: OptionStatusMap, value: string): OptionStatus[][] {
+  let cascades: OptionStatus[][] = []
+  const option = map.get(value)
+  if (option) {
+    const childrenOptions: OptionStatus[] = []
+    for (const childValue of option.childrenValues) {
+      const childOption = map.get(childValue)
+      if (childOption) {
+        childrenOptions.push(childOption)
+      }
+    }
+    cascades.push(childrenOptions)
+    if (option.parent) {
+      cascades = [...generateChildrenCascades(map, option.parent), ...cascades]
+    }
+  }
+  return cascades
+}
+
+function getChildrenOptions(map: OptionStatusMap, value: string): OptionStatus[] {
+  const children: OptionStatus[] = []
+  const option = map.get(value)
+  if (option) {
+    for (const childValue of option.childrenValues) {
+      const childOption = map.get(childValue)
+      if (childOption) {
+        children.push(childOption)
+      }
+    }
+  }
+  return children
 }
--- packages/core/src/components/cascade-select/cascade-select-panel.props.ts
+++ packages/core/src/components/cascade-select/cascade-select-panel.props.ts
@@ -7,15 +7,16 @@ export interface CascadeSelectOption {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
   label?: JSX.Element
-  value?: string
+  value: string
   children?: CascadeSelectOption[]
   disabled?: boolean
 }
 
 export interface CascadeSelectPanelProps extends ComponentProps<HTMLDivElement> {
+  value?: string
   options?: CascadeSelectOption[]
 }
 
 export function generateProps(propsRaw: CascadeSelectPanelProps) {
-  return splitProps(mergeProps({ }, propsRaw), customEventHandlersName)
+  return splitProps(mergeProps({ options: [] }, propsRaw), customEventHandlersName)
 }
