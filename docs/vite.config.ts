01304cc8e4a51d98b2d737d148e8e58945e6b860
JHFeng <lingj.jhf@outlook.com>
2023-12-05 16:54:03 +0800

    更新docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,8 +1,8 @@
 import path from 'path'
 
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import Solid from 'vite-plugin-solid'
-import UnoCSS from 'unocss/vite'
 
 function resolve(p: string) {
   return path.resolve(__dirname, p)
