92fe483fd0ffb7a6699048eb234d00a886903917
JHFeng <lingj.jhf@outlook.com>
2023-12-09 11:07:14 +0800

    DateRangePanel

--- packages/ui/src/components/date-panel/DateRangePanel.tsx
+++ packages/ui/src/components/date-panel/DateRangePanel.tsx
@@ -1,45 +1,54 @@
 import dayjs from 'dayjs'
-import { createEffect, createSignal, on } from 'solid-js'
+import { createEffect, createSignal, createContext, useContext, on } from 'solid-js'
 
-import { DatePanel } from './DatePanel'
+import { DateRangeProviderValue, DateRange, DateRangePanelProps, generateProps } from './date-range-context.props'
 import { getDateKey } from '../../utils'
-import { SpButton } from '../button'
 
-interface DateRange {
-  start?: dayjs.Dayjs
-  end?: dayjs.Dayjs
+const _DateRangeContext = createContext<DateRangeProviderValue>()
+
+export const useDateRangeContext = () => {
+  const context = useContext(_DateRangeContext)
+  if (!context) {
+    throw Error('DateRangePanelContext is undefined')
+  }
+  return context
 }
 
-export const DateRangePanel = () => {
+export const DateRangeContext = (propsRaw: DateRangePanelProps) => {
+  const props = generateProps(propsRaw)
 
   const [startMonth, setStartMonth] = createSignal(dayjs())
-  const [endMonth, setEndMonth] = createSignal(dayjs().add(1, 'month'))
+  const [endMonth, setEndMonth] = createSignal(startMonth().add(1, 'month'))
   const [dateRange, setDateRange] = createSignal<DateRange>({})
   const [dateRangeDates, setDateRangeDates] = createSignal<Record<string, dayjs.Dayjs>>({})
   const [hoverEndDate, setHoverEndDate] = createSignal<dayjs.Dayjs>()
 
   const isSelected = (date: dayjs.Dayjs) => {
     const range = dateRange()
-    return range.start && range.start.isSame(date, 'date') || range.end && range.end.isSame(date, 'date')
+    return (range.start && range.start.isSame(date, 'date') || range.end && range.end.isSame(date, 'date')) ? true : false
   }
-  const isInStartRange = (date: dayjs.Dayjs) => dateRangeDates()[getDateKey(date)]?.isSame(startMonth(), 'month') ? true : false
-  const isInEndRange = (date: dayjs.Dayjs) => dateRangeDates()[getDateKey(date)]?.isSame(endMonth(), 'month') ? true : false
+  const isHoverEnd = (date: dayjs.Dayjs) => date.isSame(hoverEndDate(), 'date')
 
   createEffect(on([startMonth, endMonth], ([startMonth, endMonth]) => {
     const range = dateRange()
     if (range.start && range.end) {
       if (range.start.isSame(range.end, 'month')) {
-        setDateRangeDates(getDateRangeDates(generateDateRangeDates(range.start, range.end)))
-        return
+        return setDateRangeDates(getDateRangeDates(generateDateRangeDates(range.start, range.end)))
+      }
+      if (startMonth.isSame(range.end, 'month')) {
+        return setDateRangeDates(getDateRangeDates(generateDateRangeDates(range.end.set('date', 1), range.end)))
+      }
+      if (endMonth.isSame(range.start, 'month')) {
+        return setDateRangeDates(getDateRangeDates(generateDateRangeDates(range.start, getMonthLastDate(range.start))))
       }
       let dates: dayjs.Dayjs[] = []
       if (startMonth.isAfter(range.start, 'month')) {
-        dates = [...generateDateRangeDates(startMonth.set('date', 1), startMonth.set('date', 1).add(1, 'month').subtract(1, 'day'))]
+        dates = [...generateDateRangeDates(startMonth.set('date', 1), getMonthLastDate(startMonth))]
       } else if (startMonth.isSame(range.start, 'month')) {
-        dates = [...generateDateRangeDates(range.start, startMonth.set('date', 1).add(1, 'month').subtract(1, 'day'))]
+        dates = [...generateDateRangeDates(range.start, getMonthLastDate(startMonth))]
       }
       if (endMonth.isBefore(range.end, 'month')) {
-        dates = [...dates, ...generateDateRangeDates(endMonth.set('date', 1), endMonth.set('date', 1).add(1, 'month').subtract(1, 'day'))]
+        dates = [...dates, ...generateDateRangeDates(endMonth.set('date', 1), getMonthLastDate(endMonth))]
       } else if (endMonth.isSame(range.end, 'month')) {
         dates = [...dates, ...generateDateRangeDates(endMonth.set('date', 1), range.end)]
       }
@@ -48,11 +57,11 @@ export const DateRangePanel = () => {
   }))
 
   function getDateRangeDates(dates: dayjs.Dayjs[]): Record<string, dayjs.Dayjs> {
-    const d: Record<string, dayjs.Dayjs> = {}
+    const record: Record<string, dayjs.Dayjs> = {}
     for (const date of dates) {
-      d[getDateKey(date)] = date
+      record[getDateKey(date)] = date
     }
-    return d
+    return record
   }
 
   function onHoverDate(date: dayjs.Dayjs) {
@@ -64,26 +73,12 @@ export const DateRangePanel = () => {
       const dates = startDate.isSame(endDate, 'month') ?
         generateDateRangeDates(startDate, endDate) :
         [
-          ...generateDateRangeDates(startDate, startDate.set('date', 1).add(1, 'month').subtract(1, 'day')),
+          ...generateDateRangeDates(startDate, getMonthLastDate(startDate)),
           ...generateDateRangeDates(endDate.set('date', 1), endDate),
         ]
       setDateRangeDates(getDateRangeDates(dates))
       setHoverEndDate(endDate)
     }
-  }
-
-  function onSelectedStartDate(date: dayjs.Dayjs) {
-    if (date.isBefore(endMonth(), 'month')) {
-      setStartMonth(value => value.set('year', date.year()).set('month', date.month()))
-    }
-    onSelectedDate(date)
-  }
-
-  function onSelectedEndDate(date: dayjs.Dayjs) {
-    if (date.isAfter(startMonth(), 'month')) {
-      setEndMonth(value => value.set('year', date.year()).set('month', date.month()))
-    }
-    onSelectedDate(date)
   }
 
   function onSelectedDate(date: dayjs.Dayjs) {
@@ -108,62 +103,20 @@ export const DateRangePanel = () => {
   }
 
   return (
-    <div class='sp-date-panel-range'>
-      <DatePanel
-        currentMonth={startMonth()}
-        max={endMonth().subtract(1, 'month')}
-        renderDate={(date: dayjs.Dayjs) => (
-          <div
-            class='sp-date-panel-wrap'
-            classList={{
-              'in-range': isInStartRange(date),
-              end: date.isSame(hoverEndDate())
-            }}
-            onMouseEnter={[onHoverDate, date]}
-          >
-            <SpButton
-              class='sp-date-panel-day'
-              classList={{
-                'in-month': date.month() === startMonth().month(),
-                today: date.isSame(dayjs(), 'date'),
-                selected: isSelected(date),
-              }}
-              onClick={[onSelectedStartDate, date]}
-            >
-              {date.date()}
-            </SpButton>
-          </div>
-        )}
-        currentMonthChange={setStartMonth}
-      />
-      <DatePanel
-        currentMonth={endMonth()}
-        min={startMonth().add(1, 'month')}
-        renderDate={(date: dayjs.Dayjs) => (
-          <div
-            class='sp-date-panel-wrap'
-            classList={{
-              'in-range': isInEndRange(date),
-              end: date.isSame(hoverEndDate())
-            }}
-            onMouseEnter={[onHoverDate, date]}
-          >
-            <SpButton
-              class='sp-date-panel-day'
-              classList={{
-                'in-month': date.month() === endMonth().month(),
-                today: date.isSame(dayjs(), 'date'),
-                selected: isSelected(date),
-              }}
-              onClick={[onSelectedEndDate, date]}
-            >
-              {date.date()}
-            </SpButton>
-          </div>
-        )}
-        currentMonthChange={setEndMonth}
-      />
-    </div>
+    <_DateRangeContext.Provider value={{
+      startMonth,
+      setStartMonth,
+      endMonth,
+      setEndMonth,
+      dateRange,
+      dateRangeDates,
+      isSelected,
+      isHoverEnd,
+      onHoverDate,
+      onSelectedDate
+    }}>
+      {props.children}
+    </_DateRangeContext.Provider>
   )
 }
 
@@ -174,4 +127,8 @@ function generateDateRangeDates(start: dayjs.Dayjs, end: dayjs.Dayjs): dayjs.Day
   }
   dates.push(end)
   return dates
+}
+
+function getMonthLastDate(date: dayjs.Dayjs): dayjs.Dayjs {
+  return date.set('date', 1).add(1, 'month').subtract(1, 'day')
 }
